e7dbe4719e5cb79d0c8317fedd91678968d96161
Jose Domingo Plaza <jdplaza@gmail.com>
2024-11-18 13:37:16 -0300

    profilescreen

--- hybrid-frontend/screens/ProfileScreen.jsx
+++ hybrid-frontend/screens/ProfileScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ActivityIndicator, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
